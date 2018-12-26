d0a8e8514217e8b1473ce998738be8c4abf32fb1
KarinGarciaZ <oscarin962011@hotmail.com>
2018-12-25 21:08:49 -0600

    Header component developed.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import logo from '../img/logo.png';
+import logobbc from '../img/logo-bbc.png';
+import logobi from '../img/logo-bi.png';
+import logoforbes from '../img/logo-forbes.png';
+import logotc from '../img/logo-techcrunch.png';
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        1
+      <div className='header'>
+        <img src={logo} alt='Nexter Logo' className='header__logo' />
+        <h3 className='heading-3'>Your own home:</h3>
+        <h1 className='heading-1'>The ultimate personal freedom</h1>
+        <button className='btn header__btn'>View our properties</button>
+        <div className='header__seenon-text'>Seen on</div>
+        <div className='header__seenon-logos'>
+          <img src={logobbc} alt='Nexter Logo' />
+          <img src={logoforbes} alt='Nexter Logo' />
+          <img src={logotc} alt='Nexter Logo' />          
+          <img src={logobi} alt='Nexter Logo' />
+        </div>
       </div>
     )
   }
