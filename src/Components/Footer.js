1b4a5dff6d8aba5f29c124e4f86d9c6181c23dbc
KarinGarciaZ <oscarin962011@hotmail.com>
2018-12-25 21:08:19 -0600

    Footer component developed.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        1
+      <div className='footer'>
+        <ul className='nav'>
+          <li className='nav__item'><a href='#' className='nav__link'>Find your dream home</a></li>
+          <li className='nav__item'><a href='#' className='nav__link'>Request Proposal</a></li>
+          <li className='nav__item'><a href='#' className='nav__link'>Download home planer</a></li>
+          <li className='nav__item'><a href='#' className='nav__link'>Contact us</a></li>
+          <li className='nav__item'><a href='#' className='nav__link'>Submit your property</a></li>
+          <li className='nav__item'><a href='#' className='nav__link'>Come work with us!</a></li>
+        </ul>
+        <p className='copyright'>
+          &copy; Copyright 2018 by Karin García.
+        </p>
       </div>
     )
   }
